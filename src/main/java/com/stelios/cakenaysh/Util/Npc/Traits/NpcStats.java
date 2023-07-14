d7601a932c29a3e5f1acc5de5326892f07377389
Stelios D <snd321321snd@gmail.com>
2023-07-14 19:58:54 +0300

    Move the listener to give xp on NPC death to a different class.

--- src/main/java/com/stelios/cakenaysh/Util/Npc/Traits/NpcStats.java
+++ src/main/java/com/stelios/cakenaysh/Util/Npc/Traits/NpcStats.java
@@ -1,15 +1,8 @@
 package com.stelios.cakenaysh.Util.Npc.Traits;
 
 import com.stelios.cakenaysh.Main;
-import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.persistence.Persist;
 import net.citizensnpcs.api.trait.Trait;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.mcmonkey.sentinel.SentinelTrait;
 
 public class NpcStats extends Trait {
     public NpcStats() {
@@ -192,31 +185,5 @@ public class NpcStats extends Trait {
         magicDefense = 0.0f;
         magicDamage = 0.0f;
     }
-
-    //giving xp to the player that killed the npc
-    @EventHandler
-    public void onPlayerDeath(EntityDeathEvent e){
-
-        //if the player is a npc with the sentinel and npc stats trait return
-        if(CitizensAPI.getNPCRegistry().isNPC(e.getEntity()) && CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).hasTrait(SentinelTrait.class) && CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).hasTrait(NpcStats.class)){
-
-            try {
-                //if the killer is a player and not a npc
-                if (e.getEntity().getKiller() instanceof Player && !CitizensAPI.getNPCRegistry().isNPC(e.getEntity().getKiller())) {
-
-                    //get the player
-                    Player player = e.getEntity().getKiller();
-
-                    //add xp to the player
-                    main.getPlayerManager().getCustomPlayer(player.getUniqueId()).addXp((int) CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp());
-                    player.sendMessage(Component.text("+" + (int) CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp() + "XP", TextColor.color(0, 255, 0)));
-                }
-            }catch (NullPointerException ex){
-                //do nothing
-            }
-        }
-
-    }
-
 
 }
