ee0720b8c8d4e74bd62ae087caedb03c0323234c
Stelios D <snd321321snd@gmail.com>
2023-07-14 16:22:09 +0300

    Slightly altered the EntityDeathEvent (it still fires 3 times for some reason) to not produce an error in the console every time.

--- src/main/java/com/stelios/cakenaysh/Util/Npc/Traits/NpcStats.java
+++ src/main/java/com/stelios/cakenaysh/Util/Npc/Traits/NpcStats.java
@@ -8,7 +8,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.mcmonkey.sentinel.SentinelTrait;
 
 public class NpcStats extends Trait {
@@ -195,25 +195,25 @@ public class NpcStats extends Trait {
 
     //giving xp to the player that killed the npc
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent e){
+    public void onPlayerDeath(EntityDeathEvent e){
 
         //if the player is a npc with the sentinel and npc stats trait return
         if(CitizensAPI.getNPCRegistry().isNPC(e.getEntity()) && CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).hasTrait(SentinelTrait.class) && CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).hasTrait(NpcStats.class)){
 
-            //if the killer is null return
-            if(e.getEntity().getKiller() != null) return;
+            try {
+                //if the killer is a player and not a npc
+                if (e.getEntity().getKiller() instanceof Player && !CitizensAPI.getNPCRegistry().isNPC(e.getEntity().getKiller())) {
 
-            //if the killer is a player and not a npc
-            if(e.getEntity().getKiller() instanceof Player && !CitizensAPI.getNPCRegistry().isNPC(e.getEntity().getKiller())){
+                    //get the player
+                    Player player = e.getEntity().getKiller();
 
-                //get the player
-                Player player = e.getEntity().getKiller();
-
-                //add xp to the player
-                main.getPlayerManager().getCustomPlayer(player.getUniqueId()).addXp((int) CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp());
-                player.sendMessage(Component.text("+" + CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp() + "XP", TextColor.color(0, 255, 0)));
+                    //add xp to the player
+                    main.getPlayerManager().getCustomPlayer(player.getUniqueId()).addXp((int) CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp());
+                    player.sendMessage(Component.text("+" + (int) CitizensAPI.getNPCRegistry().getNPC(e.getEntity()).getOrAddTrait(NpcStats.class).getXp() + "XP", TextColor.color(0, 255, 0)));
+                }
+            }catch (NullPointerException ex){
+                //do nothing
             }
-
         }
 
     }
