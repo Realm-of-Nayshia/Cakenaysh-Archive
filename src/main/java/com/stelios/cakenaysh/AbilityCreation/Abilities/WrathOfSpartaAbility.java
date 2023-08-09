484e08b87fac49392adaf349eba7edd4450435bf
Stelios D <snd321321snd@gmail.com>
2023-08-09 21:38:36 +0300

    Made the wrath of sparta ability double the player's damage.

--- src/main/java/com/stelios/cakenaysh/AbilityCreation/Abilities/WrathOfSpartaAbility.java
+++ src/main/java/com/stelios/cakenaysh/AbilityCreation/Abilities/WrathOfSpartaAbility.java
@@ -3,10 +3,13 @@ package com.stelios.cakenaysh.AbilityCreation.Abilities;
 import com.stelios.cakenaysh.AbilityCreation.ItemAbility;
 import com.stelios.cakenaysh.AbilityCreation.CustomAbilities;
 import com.stelios.cakenaysh.Items.Item;
+import com.stelios.cakenaysh.Main;
+import com.stelios.cakenaysh.Util.CustomPlayer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class WrathOfSpartaAbility extends ItemAbility {
 
@@ -16,13 +19,24 @@ public class WrathOfSpartaAbility extends ItemAbility {
 
     public void doAbility(Player player){
 
-        //get the main class
-        //Main main = Main.getPlugin(Main.class);
+        CustomPlayer customPlayer = Main.getPlugin(Main.class).getPlayerManager().getCustomPlayer(player.getUniqueId());
+
+        float bonusDamage = customPlayer.getBonusDamage() + customPlayer.getDamage();
+        customPlayer.setBonusDamage(bonusDamage);
 
         player.sendMessage(Component.text("The spark within you ignites... Rage takes over...",
                 TextColor.color(255,0,0)));
         player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 15, 15);
-        //main.getPlayerManager().getCustomPlayer(player.getUniqueId()).add
+
+        //wait 10 seconds
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.sendMessage(Component.text("The rage subsides...",
+                        TextColor.color(255,0,0)));
+                customPlayer.setBonusDamage(customPlayer.getBonusDamage() - bonusDamage);
+            }
+        }.runTaskLaterAsynchronously(Main.getPlugin(Main.class), 200);
 
     }
 }
