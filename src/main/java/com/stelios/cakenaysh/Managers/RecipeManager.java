def34cde6dffc01a96dcda4301bb59e0a1aca6c3
Stelios D <snd321321snd@gmail.com>
2023-08-12 14:32:56 +0300

    Refactored the recipe yml file to hold the key part of each recipe's NamespacedKey instead of holding the key itself. Also added a few new methods to reset and give all recipes.

--- src/main/java/com/stelios/cakenaysh/Managers/RecipeManager.java
+++ src/main/java/com/stelios/cakenaysh/Managers/RecipeManager.java
@@ -1,5 +1,6 @@
 package com.stelios.cakenaysh.Managers;
 
+import com.stelios.cakenaysh.Items.Recipes;
 import com.stelios.cakenaysh.Main;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -18,37 +19,63 @@ public class RecipeManager {
 
 
     //get the player's list of recipes
-    public ArrayList<NamespacedKey> getRecipes(Player player){
-        return (ArrayList<NamespacedKey>) config.getList(player.getUniqueId().toString());
+    public ArrayList<String> getRecipes(Player player){
+
+        ArrayList<String> recipes = (ArrayList<String>) config.getList(player.getUniqueId().toString());
+
+        if (recipes == null){
+            recipes = new ArrayList<>();
+        }
+
+        return recipes;
     }
 
     //add a recipe to the player's list
-    public void addRecipe(Player player, NamespacedKey recipe){
+    public void addRecipe(Player player, String recipe){
 
         //current recipes
-        ArrayList<NamespacedKey> recipes = getRecipes(player);
+        ArrayList<String> recipes = getRecipes(player);
         recipes.add(recipe);
 
         config.set(player.getUniqueId().toString(), recipes);
         save();
-        player.discoverRecipe(recipe);
+        player.discoverRecipe(new NamespacedKey(main, recipe));
     }
 
     //remove a recipe from the player's list
-    public void removeRecipe(Player player, NamespacedKey recipe){
+    public void removeRecipe(Player player, String recipe){
 
         //current recipes
-        ArrayList<NamespacedKey> recipes = getRecipes(player);
+        ArrayList<String> recipes = getRecipes(player);
         recipes.remove(recipe);
 
         config.set(player.getUniqueId().toString(), recipes);
         save();
-        player.undiscoverRecipe(recipe);
+        player.undiscoverRecipe(new NamespacedKey(main, recipe));
     }
 
     //check if the player has a recipe
-    public boolean hasRecipe(Player player, NamespacedKey recipe){
+    public boolean hasRecipe(Player player, String recipe){
         return getRecipes(player).contains(recipe);
+    }
+
+    //add all recipes to the player
+    public void addAllRecipes(Player player){
+
+        for (Recipes recipe : Recipes.values()) {
+            if (!hasRecipe(player, recipe.getKey().getKey())) {
+                addRecipe(player, recipe.getKey().getKey());
+            }
+        }
+    }
+
+    //reset all recipes from the player
+    public void resetRecipes(Player player){
+        for (Recipes recipe : Recipes.values()) {
+            if (hasRecipe(player, recipe.getKey().getKey())) {
+                removeRecipe(player, recipe.getKey().getKey());
+            }
+        }
     }
 
     //save the file
