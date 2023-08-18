58284451666a188b98224c0f9770de7a41af5c4b
Stelios D <snd321321snd@gmail.com>
2023-08-18 09:06:40 +0300

    Added quests as a new collection in the database to be used at a future point in time.

--- src/main/java/com/stelios/cakenaysh/Util/Database.java
+++ src/main/java/com/stelios/cakenaysh/Util/Database.java
@@ -11,18 +11,20 @@ public class Database {
     private final MongoClient client;
     private final MongoDatabase database;
     private final MongoCollection<Document> playerStats;
-    private final MongoCollection<Document> playerItems;
-    private final MongoCollection<Document> playerStashes;
+    private final MongoCollection<Document> playerQuests;
     private final MongoCollection<Document> playerRecipes;
+    private final MongoCollection<Document> playerStashes;
+    private final MongoCollection<Document> playerItems;
     private final MongoCollection<Document> npcInfo;
 
     public Database() {
         client = MongoClients.create();
         database = client.getDatabase("cakenaysh");
         playerStats = database.getCollection("playerStats");
-        playerItems = database.getCollection("playerItems");
-        playerStashes = database.getCollection("playerStashes");
+        playerQuests = database.getCollection("playerQuests");
         playerRecipes = database.getCollection("playerRecipes");
+        playerStashes = database.getCollection("playerStashes");
+        playerItems = database.getCollection("playerItems");
         npcInfo = database.getCollection("npcInfo");
     }
 
@@ -36,14 +38,17 @@ public class Database {
     public MongoCollection<Document> getPlayerStats() {
         return playerStats;
     }
-    public MongoCollection<Document> getPlayerItems() {
-        return playerItems;
+    public MongoCollection<Document> getPlayerQuests() {
+        return playerQuests;
+    }
+    public MongoCollection<Document> getPlayerRecipes() {
+        return playerRecipes;
     }
     public MongoCollection<Document> getPlayerStashes() {
         return playerStashes;
     }
-    public MongoCollection<Document> getPlayerRecipes() {
-        return playerRecipes;
+    public MongoCollection<Document> getPlayerItems() {
+        return playerItems;
     }
     public MongoCollection<Document> getNpcInfo() {
         return npcInfo;
